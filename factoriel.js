18962474eb0d3f4031b02b164a3390f3f658ae4d
AlexisAbalea <abaleaalexis@gmail.com>
2021-12-06 16:20:51 +0100

    ajout controle input factoriel

--- factoriel.js
+++ factoriel.js
@@ -1,17 +1,20 @@
 // On récupere l'argument
 const argument = process.argv[2];
 
-// check argument existant
-if (!argument) {
-    console.log('Merci de saisir un nombre.');
-} 
-
-factoriel = (nombre) => {
+factoriel = (nombreString) => {
+    const nombreInt = parseInt(nombreString);
+    if (nombreInt === 0) return 1;
     // On créé un tableau de dimension n, et on recupere son index +1 en valeur
-    tableauNombres = Array.from(Array(parseInt(nombre)).keys()).map(k => k + 1);
+    tableauNombres = Array.from(Array(parseInt(nombreInt)).keys()).map(k => k + 1);
     // On multiplie chaque valeur du tableau par le suivant.
     return tableauNombres.reduce((nbrPrecedent, nbrSuivant) => nbrPrecedent * nbrSuivant);
 }
 
-const resultat = factoriel(argument);
-console.log('La factorielle de ' + argument + ' est ' +resultat +'.');
+// check de securité de l'input
+if (!argument || isNaN(argument)) {
+    console.log('Merci de saisir un nombre.');
+} else if (argument < 0) {
+    console.log('Merci de saisir un nombre positif.');
+} else {
+    console.log('La factorielle de ' + argument + ' est ' + factoriel(argument) +'.');
+}
